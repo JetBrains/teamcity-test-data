b4714c9d9c50d71c3b2789e4c171081a4c780163
Igor Emtsev <igor.emtsev@jetbrains.com>
2019-11-07 21:34:04 +0300

    generated-tests: Updated settings DSL

--- generated-tests/.teamcity/settings.kts
+++ generated-tests/.teamcity/settings.kts
@@ -1,5 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.exec
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 
 /*
@@ -7,12 +7,17 @@ The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
 project() function with a Project instance or an init function as
 an argument.
+
 VcsRoots, BuildTypes, Templates, and subprojects can be
 registered inside the project using the vcsRoot(), buildType(),
 template(), and subProject() methods respectively.
+
 To debug settings scripts in command-line, run the
+
     mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
 command and attach your debugger to the port 8000.
+
 To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 -> Tool Windows -> Maven Projects), find the generate task node
 (Plugins -> teamcity-configs -> teamcity-configs:generate), the
@@ -23,33 +28,38 @@ version = "2019.1"
 
 project {
 
-    vcsRoot(HttpsGheLabsIntellijNetTeamcityUserTestDataGit)
+    vcsRoot(HttpsGithubComJetBrainsTeamcityTestDataGit)
+    vcsRoot(HttpsGithubComGarics2000tempConfigGit)
 
-    buildType(RunTests)
+    buildType(Test)
 }
 
-object RunTests : BuildType({
-    name = "Run tests"
+object Test : BuildType({
+    name = "Test"
+
+    params {
+        param("system.ALL_TESTS_NUM", "10000")
+        param("system.TEST_LOG_LINES_NUM", "100")
+        param("system.FAILED_TESTS_PERCENTAGE", "30")
+    }
 
     vcs {
-        root(HttpsGheLabsIntellijNetTeamcityUserTestDataGit, "generated-tests =>")
+        root(HttpsGithubComJetBrainsTeamcityTestDataGit, " generated-tests =>")
     }
 
     steps {
-        exec {
-            name = "Fake tests"
-            executionMode = BuildStep.ExecutionMode.ALWAYS
-            path = "./generate_fake_run.sh"
-            arguments = "12000"
+        script {
+            scriptContent = "./generate_fake_run.sh %system.ALL_TESTS_NUM% %system.FAILED_TESTS_PERCENTAGE% %system.TEST_LOG_LINES_NUM%"
         }
     }
 })
 
-object HttpsGheLabsIntellijNetTeamcityUserTestDataGit : GitVcsRoot({
-    name = "test-data-repo"
-    url = "https://ghe.labs.intellij.net/teamcity-user/test-data.git"
-    authMethod = password {
-        userName = "teamcity-user"
-        password = "credentialsJSON:13a671a2-b7f7-4c5d-bbf5-84ccfe3cf7cc"
-    }
+object HttpsGithubComGarics2000tempConfigGit : GitVcsRoot({
+    name = "https://github.com/Garics2000/temp-config.git"
+    url = "https://github.com/Garics2000/temp-config.git"
+})
+
+object HttpsGithubComJetBrainsTeamcityTestDataGit : GitVcsRoot({
+    name = "https://github.com/JetBrains/teamcity-test-data.git"
+    url = "https://github.com/JetBrains/teamcity-test-data.git"
 })
